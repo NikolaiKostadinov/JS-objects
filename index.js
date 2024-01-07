6d09dce74f6e4a221a450ed6b0eae3b1cccf6606
NikolaiKostadinov <nikolaikostadinov123@abv.bg>
2024-01-07 17:00:19 +0200

    object

---
+++ index.js
@@ -0,0 +1,13 @@
+const person1 = {
+    firstName: "Jhon" ,
+    lastName: "Anderson" ,
+    age: 40 ,
+    isItHuman: true,
+    
+}
+
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isItHuman);
